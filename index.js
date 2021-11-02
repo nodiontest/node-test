a56ecc62601c3b59511129820e81dedbdca10766
nodiontest <90258784+nodiontest@users.noreply.github.com>
2021-11-02 13:32:52 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello Nodion!!')
+  res.send('Hello Nodion!! Ich bin ein Update.')
 })
 
 app.listen(port, () => {
